a064e83dfb889a3dffe15e7a1594ba3d7d386a30
Oliver Fleckenstein <oliverfl@live.dk>
2016-02-24 10:46:24 +0100

    Agent01

---
+++ src/smith/Agent01.java
@@ -0,0 +1,29 @@
+package smith;
+
+import ch.idsia.agents.Agent;
+import ch.idsia.agents.controllers.BasicMarioAIAgent;
+import ch.idsia.benchmark.mario.engine.sprites.Mario;
+import ch.idsia.benchmark.mario.environments.Environment;
+
+public class Agent01 extends BasicMarioAIAgent implements Agent {
+
+	public Agent01() {
+		super("Agent01");
+		// TODO Auto-generated constructor stub
+	}
+
+	public void reset()
+	{
+	    action = new boolean[Environment.numberOfKeys];
+	}
+	
+	public boolean[] getAction()
+	{
+		reset();
+		if (isMarioAbleToJump)
+			action[Mario.KEY_JUMP] = true;
+		else 
+			action[Mario.KEY_JUMP] = false;
+		return action;
+	}
+}
