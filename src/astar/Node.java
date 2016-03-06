749dc000a0e019c166191e4bcc8913bc72ed26cd
Oliver Fleckenstein <oliverfl@live.dk>
2016-03-06 18:51:02 +0100

    Updates to node class

--- src/astar/Node.java
+++ src/astar/Node.java
@@ -6,13 +6,16 @@ import ch.idsia.benchmark.mario.engine.sprites.Mario;
 import ch.idsia.benchmark.mario.engine.sprites.Sprite;
 
 public class Node {
-
-	int fitness;		// Overall rating of this option 
-	int depth;			// Depth of the current node
+	// Coordinates of the node
+	private float x, y;
+	
+	int fitness;				// Overall rating of this option 
+	int depth;					// Depth of the current node
+	private boolean[] action;	// Action that are done in this node
 	
 	// Game elements 
-	public Mario mario;
-	int damageTaken;
+	public Mario mario;			// The Mario object 
+	int damageTaken;			// How much damage that Mario will take in this step 
 	
 	// Enemies 
 	public List<Sprite> enemies;
@@ -24,9 +27,12 @@ public class Node {
 	public List<Node> children;
 	
 	// Should have everything needed to compute next frame
-	public Node(byte[][] levelScene) 
+	public Node(byte[][] levelScene, Mario mario, List<Sprite> enemies, boolean[] action) 
 	{
 		this.levelScene = levelScene;
+		this.mario = mario; 
+		this.enemies = enemies;
+		this.action = action;
 	}
 	
 	// The function to evaluate the current frame
@@ -39,6 +45,20 @@ public class Node {
 	// Should return the best option found 
 	public boolean[] searchChildren()
 	{
-		return null;
+		// Base case: If no children, return this node's actions 
+		if (children == null) return this.action;
+		
+		int max = Integer.MIN_VALUE;
+		boolean[] childAction;
+		for (Node node : children) 
+		{
+			if (node.fitness > max)
+			{
+				max = node.fitness;
+				childAction = node.searchChildren();
+			}
+		}
+		
+		return this.action;
 	}
 }
